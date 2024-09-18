23d3253a5718d080b6f7cd6b689c5d51d7edddba
AYTC <aytacalir@gmail.com>
2024-09-18 20:31:44 +0300

    router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+
+//!en dış sarmala BrowserRouter tanımlaması yapılır
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+
+);
